74e5e409855c7db4c935b98b4c349a8afda83c65
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-04-26 12:48:50 -0300

    cargar atributos y metodos

--- Disquera.php
+++ Disquera.php
@@ -1,2 +1,61 @@
 <?php
 
+class Disquera{
+    private $hora_desde;
+    private $hora_hasta;
+    private $estado;
+    private $direccion;
+    private $Dueño;
+
+    public function __construct($desde, $hasta,$state, $address,$owner){
+        $this->hora_desde = $desde;
+        $this->hora_hasta = $hasta;
+        $this->estado = $state;
+        $this->direccion = $address;
+        $this->Dueño = $owner;
+    }
+    
+    
+        
+    
+
+    public function getHora_desde(){
+        return $this->hora_desde;
+    }
+
+    public function setHora_desde($desde){
+        $this->hora_desde = $desde;
+    }
+
+    public function getHora_hasta(){
+        return $this->hora_hasta;
+    }
+
+    public function setHora_hasta($hasta){
+        $this->hora_hasta = $hasta;
+    }
+
+    public function getEstado(){
+        return $this->estado;
+    }
+
+    public function setEstado($state){
+        $this->estado = $state;
+    }
+
+    public function getDireccion(){
+        return $this->direccion;
+    }
+
+    public function setDireccion($address){
+        $this->direccion = $address;
+    }
+
+    public function getDueño(){
+        return $this->Dueño;
+    }
+
+    public function setDueño($owner){
+        $this->Dueño = $owner;
+    }
+}
